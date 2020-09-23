a64d3a115ff8497532c4c9c937bfeaacc7bf3daa
chmh0805 <63082842+chmh0805@users.noreply.github.com>
2020-09-23 22:18:00 +0900

    4_Keyboard event
    1. 키보드 방향키 입력 시  # event.type == pygame.KEYDOWN:
    2. 입력방향에 따라 # if event.key == pygame.K_LEFT/RIGHT/UP/DOWN:
    3. 이동 # to_x, to_y = 0, move_pos = 5 / to_x+-= move_pos, to_y+-= move_pos
    4. 방향키를 떼면 # if event.type == pygame.KEYUP:
    5. 멈춤 # to_x = 0 or to_y = 0
    6. 캐릭터가 화면 밖으로 벗어나지 못하게 제한

--- pygame_basic.py
+++ pygame_basic.py
@@ -3,7 +3,7 @@ import pygame
 pygame.init() # 초기화 (반드시 필요)
 
 # 화면 크기 설정
-screen_width = 800 # 가로 크기
+screen_width = 480 # 가로 크기
 screen_height = 640 # 세로 크기
 screen = pygame.display.set_mode((screen_width, screen_height))
 
@@ -21,6 +21,11 @@ character_height = character_size[1] # 캐릭터의 세로 크기(190)
 character_x_pos = screen_width / 2 - character_width / 2 # 화면 가로의 절반 크기에 위치
 character_y_pos = screen_height - character_height # 화면 세로 크기 가장 아래에 위치
 
+# 이동 좌표
+to_x = 0
+to_y = 0
+move_pos = 5 # 이동하는 좌표수
+
 # 이벤트 루프
 running = True # 게임이 진행중인가를 확인
 
@@ -29,9 +34,38 @@ while running:
 		if event.type == pygame.QUIT: # 오른쪽 상단 x버튼을 누르면 종료
 			running = False
 
+		if event.type == pygame.KEYDOWN: # 키가 눌러졌는지 확인
+			if event.key == pygame.K_LEFT: # 캐릭터를 왼쪽으로
+				to_x -= move_pos # to_x = to_x - 5
+			elif event.key == pygame.K_RIGHT: # 캐릭터를 오른쪽으로
+				to_x += move_pos
+			elif event.key == pygame.K_UP: # 캐릭터를 위로
+				to_y -= move_pos
+			elif event.key == pygame.K_DOWN: # 캐릭터를 아래로
+				to_y += move_pos
+
+		if event.type == pygame.KEYUP: # 방향키를 떼면 멈춤
+			if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+				to_x = 0
+			elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+				to_y = 0
+
+	character_x_pos += to_x
+	character_y_pos += to_y
+
+	if character_x_pos < 0:
+		character_x_pos = 0
+	elif character_x_pos > screen_width - character_width:
+		character_x_pos = screen_width - character_width
+
+	if character_y_pos < 0:
+		character_y_pos = 0
+	elif character_y_pos > screen_height - character_height:
+		character_y_pos = screen_height - character_height
+
 	screen.blit(background, (0, 0)) # 배경그리기 .blit(배경사진, (가로, 세로))
 
-	screen.blit(character, (character_x_pos, character_y_pos))
+	screen.blit(character, (character_x_pos, character_y_pos)) # 캐릭터 그리기
 
 	pygame.display.update() # 게임 화면을 계속해서 다시 그리기!
 
