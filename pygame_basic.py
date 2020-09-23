d3c6bdbc9ec4f81410a54fe62fa2cfba90e8433a
chmh0805 <63082842+chmh0805@users.noreply.github.com>
2020-09-23 22:27:35 +0900

    5_frame_per_second
    1. 이동속도 변수명 변경
    2. FPS 설정 # clock = pygame.time.Clock()
    3. 게임 화면의 초당 프레임 수를 설정 # dt = clock.tick(60)
    4. 이동속도를 0.3, 방향키 입력 시 이동거리를 to_x,to_y * dt로 변경
    - fps가 달라져도 이동속도가 동일함.

--- pygame_basic.py
+++ pygame_basic.py
@@ -10,6 +10,9 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 # 화면 타이틀 설정
 pygame.display.set_caption("Nado Game") # 게임 이름
 
+# FPS
+clock = pygame.time.Clock()
+
 # 배경 이미지 불러오기
 background = pygame.image.load("background.png")
 
@@ -24,25 +27,33 @@ character_y_pos = screen_height - character_height # 화면 세로 크기 가장
 # 이동 좌표
 to_x = 0
 to_y = 0
-move_pos = 5 # 이동하는 좌표수
+
+# 이동 속도
+charater_speed = 0.6 # 이동하는 좌표수
 
 # 이벤트 루프
 running = True # 게임이 진행중인가를 확인
 
 while running:
+	dt = clock.tick(60) # 게임 화면의 초당 프레임 수를 설정
+
+# 캐릭터가 100만큼 이동을 해야함
+# 10fps : 1초 동안에 10번 동작 -> 한번에 10만큼 이동
+# 20fps : 1초 동안에 20번 동작 -> 한번에 5만큼 이동
+
 	for event in pygame.event.get(): # 어떤 이벤트가 발생하였는지 체크
 		if event.type == pygame.QUIT: # 오른쪽 상단 x버튼을 누르면 종료
 			running = False
 
 		if event.type == pygame.KEYDOWN: # 키가 눌러졌는지 확인
 			if event.key == pygame.K_LEFT: # 캐릭터를 왼쪽으로
-				to_x -= move_pos # to_x = to_x - 5
+				to_x -= charater_speed # to_x = to_x - 5
 			elif event.key == pygame.K_RIGHT: # 캐릭터를 오른쪽으로
-				to_x += move_pos
+				to_x += charater_speed
 			elif event.key == pygame.K_UP: # 캐릭터를 위로
-				to_y -= move_pos
+				to_y -= charater_speed
 			elif event.key == pygame.K_DOWN: # 캐릭터를 아래로
-				to_y += move_pos
+				to_y += charater_speed
 
 		if event.type == pygame.KEYUP: # 방향키를 떼면 멈춤
 			if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
@@ -50,8 +61,8 @@ while running:
 			elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
 				to_y = 0
 
-	character_x_pos += to_x
-	character_y_pos += to_y
+	character_x_pos += to_x * dt
+	character_y_pos += to_y * dt
 
 	if character_x_pos < 0:
 		character_x_pos = 0
