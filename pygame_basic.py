0b3a698f5b3075271db4fd226cbf2c78503c0923
chmh0805 <63082842+chmh0805@users.noreply.github.com>
2020-09-23 22:48:31 +0900

    7_Text
    1. 폰트 정의
    2. 총시간, 시작시간 정의
    3. 경과 시간 계산
    4. 결과 시간을 표시
    5. 시간이 0초 이하이면 종료
    6. 종료 전 2초 대기

--- pygame_basic.py
+++ pygame_basic.py
@@ -39,6 +39,16 @@ enemy_height = enemy_size[1] # 캐릭터의 세로 크기(190)
 enemy_x_pos = (screen_width / 2) - (enemy_width / 2) # 화면 가로의 절반 크기에 위치
 enemy_y_pos = (screen_height / 2) - enemy_height # 화면 세로 크기 가장 아래에 위치
 
+
+# 폰트 정의
+game_font = pygame.font.Font(None, 40) # 폰트 객체 생성 (폰트, 크기)
+
+# 총 시간
+total_time = 10
+
+# 시작 시간 정보
+start_ticks = pygame.time.get_ticks() # 시작 tick을 받아옴
+
 # 이벤트 루프
 running = True # 게임이 진행중인가를 확인
 
@@ -103,7 +113,24 @@ while running:
 	screen.blit(character, (character_x_pos, character_y_pos)) # 캐릭터 그리기
 	screen.blit(enemy, (enemy_x_pos, enemy_y_pos)) # 적 캐릭터 그리기
 
+	# 타이머 집어 넣기
+	# 경과 시간 계산
+	elapsed_time = (pygame.time.get_ticks() - start_ticks) / 1000
+	# 결과 시간(ms)을 1000으로 나누어서 초(s) 단위로 표시
+
+	timer = game_font.render(str(int(total_time - elapsed_time)), True, (255, 255, 255))
+	# 출력할 글자, True, 글자 색상(rgb)
+	screen.blit(timer, (10, 10))
+
+	# 만약 시간이 0 이하이면 게임 종료
+	if total_time - elapsed_time <= 0:
+		print("타임 아웃")
+		running = False
+
 	pygame.display.update() # 게임 화면을 계속해서 다시 그리기!
+
+# 잠시 대기
+pygame.time.delay(2000) # 2초 정도 대기(ms)
 
 # pygame 종료
 pygame.quit()
